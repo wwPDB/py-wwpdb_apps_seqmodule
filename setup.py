c6f7c7d716f8566f7b9385a06e6cd8b06d04be59
Ezra Peisach <ezra.peisach@rcsb.org>
2018-11-20 23:41:48 -0500

    Update PairwiseAlignTests

---
+++ setup.py
@@ -0,0 +1,77 @@
+# File: setup.py
+# Date: 6-Oct-2018
+#
+# Update:
+#
+import re
+import glob
+
+from setuptools import find_packages
+from setuptools import setup
+
+packages = []
+thisPackage = 'wwpdb.apps.seqmodule'
+
+with open('wwpdb/apps/seqmodule/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+
+setup(
+    name=thisPackage,
+    version=version,
+    description='wwPDB sequence module',
+    long_description="See:  README.md",
+    author='Ezra Peisach',
+    author_email='ezra.peisach@rcsb.org',
+    url='https://github.com/rcsb/py-wwpdb_apps_seqmodule',
+    #
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    #
+    install_requires=['wwpdb.utils.config', 'wwpdb.io', 'wwpdb.utils.session',
+                      'wwpdb.utils.wf', 'wwpdb.utils.seqdb_v2', 'wwpdb.utils.db',
+                      'wwpdb.utils.align', 'wwpdb.utils.detach'],
+    packages=find_packages(exclude=['wwpdb.apps.tests-seqmodule',
+                                    'mock-data']),
+    # Enables Manifest to be used
+    #include_package_data = True,
+    package_data={
+        # If any package contains *.md or *.rst ...  files, include them:
+        '': ['*.md', '*.rst', "*.txt", "*.cfg"],
+    },
+    #
+    # These basic tests require no database services -
+    test_suite="wwpdb.apps.tests-seqmodule",
+    tests_require=['tox'],
+    #
+    # Not configured ...
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+    # Added for
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', thisPackage),
+            'version': ('setup.py', version),
+            'release': ('setup.py', version)
+        }
+    },
+    # This setting for namespace package support -
+    zip_safe=False,
+)
--- wwpdb/apps/tests-seqmodule/PairwiseAlignTests.py
+++ wwpdb/apps/tests-seqmodule/PairwiseAlignTests.py
@@ -10,44 +10,45 @@ Test cases for pairwise sequence alignment wrapper class.
 
 """
 __docformat__ = "restructuredtext en"
-__author__    = "John Westbrook"
-__email__     = "jwest@rcsb.rutgers.edu"
-__license__   = "Creative Commons Attribution 3.0 Unported"
-__version__   = "V0.06"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Creative Commons Attribution 3.0 Unported"
+__version__ = "V0.06"
 
-import sys, unittest, traceback
-import time, os, os.path
+import sys
+import traceback
+import unittest
 
+from wwpdb.utils.align.alignlib import PairwiseAlign
 
-from wwpdb.utils.pair_align.wrapper.libPairwiseAlignPackage import PairwiseAlign, ostream
 from wwpdb.apps.seqmodule.util.SequenceExamples import SequenceExamples
 
 
 class PairwiseAlignTests(unittest.TestCase):
     def setUp(self):
-        self.__verbose=True
-        sE=SequenceExamples()
-        self.seqRef3L=sE.getRefSequence3List('A')        
-        self.seqAuth3L=sE.getAuthSequenceList('A')
+        self.__verbose = True
+        sE = SequenceExamples()
+        self.seqRef3L = sE.getRefSequence3List('A')
+        self.seqAuth3L = sE.getAuthSequenceList('A')
         #
         # Test sequence with random insertions and deletions
         #
-        self.sTests={}
-        for tt in ['T1','T2','T3','T4','T5']:
-            self.sTests[tt]=sE.getAuthSequenceListTest('A')
+        self.sTests = {}
+        for tt in ['T1', 'T2', 'T3', 'T4', 'T5']:
+            self.sTests[tt] = sE.getAuthSequenceListTest('A')
         #
 
     def tearDown(self):
         pass
-    
-    def testAlign(self): 
+
+    def testAlign(self):
         """ Run internal alignment test embedded in the class -  This is a basic santity check.
         """
         sys.stdout.write("\n------------------------ ")
         sys.stdout.write("Starting test function  %s" % sys._getframe().f_code.co_name)
         sys.stdout.write(" -------------------------\n")
         try:
-            pA=PairwiseAlign()
+            pA = PairwiseAlign()
             pA.testExample()
         except:
             traceback.print_exc(file=sys.stdout)
@@ -61,25 +62,24 @@ class PairwiseAlignTests(unittest.TestCase):
         sys.stdout.write("Starting test function  %s" % sys._getframe().f_code.co_name)
         sys.stdout.write(" -------------------------\n")
         try:
-            pA=PairwiseAlign()
+            pA = PairwiseAlign()
             pA.setVerbose(self.__verbose)
-            pA.setReferenceSequence(self.seqRef3L,"REFA")
-            pA.addTestSequence(self.seqAuth3L,"A")
+            pA.setReferenceSequence(self.seqRef3L, "REFA")
+            pA.addTestSequence(self.seqAuth3L, "A")
             pA.doAlign()
             pA.prAlignmentConflicts("A")
             sys.stdout.write("Length of reference sequence = %d\n" % len(self.seqRef3L))
             sys.stdout.write("Length of    author sequence = %d\n" % len(self.seqAuth3L))
-            myAlign=pA.getAlignment("A")
+            myAlign = pA.getAlignment("A")
             sys.stdout.write("Length   of alignment     = %d\n" % len(myAlign))
             ii = 0
             for myPr in myAlign:
                 if myPr[0] != myPr[1]:
-                    sys.stdout.write("Py - conflict at alignment position %d  -  %s - %s\n" % (ii,myPr[0],myPr[1]))
-                ii+=1
+                    sys.stdout.write("Py - conflict at alignment position %d  -  %s - %s\n" % (ii, myPr[0], myPr[1]))
+                ii += 1
         except:
             traceback.print_exc(file=sys.stdout)
             self.fail()
-
 
     def testAlign3(self):
         """  Consensus alignment for author and reference sequences.
@@ -89,23 +89,23 @@ class PairwiseAlignTests(unittest.TestCase):
         sys.stdout.write("Starting test function  %s" % sys._getframe().f_code.co_name)
         sys.stdout.write(" -------------------------\n")
         try:
-            pA=PairwiseAlign()
-            pA.setVerbose(self.__verbose)            
-            pA.setReferenceSequence(self.seqRef3L,"REFA")
-            pA.addTestSequence(self.seqAuth3L,"A")
+            pA = PairwiseAlign()
+            pA.setVerbose(self.__verbose)
+            pA.setReferenceSequence(self.seqRef3L, "REFA")
+            pA.addTestSequence(self.seqAuth3L, "A")
             #
             myFails = pA.doAlignConsensus()
             sys.stdout.write("Failed sequences = %d\n" % len(myFails))
             pA.prAlignmentConflicts("A")
             sys.stdout.write("Length reference sequence = %d\n" % len(self.seqRef3L))
             sys.stdout.write("Length    author sequence = %d\n" % len(self.seqAuth3L))
-            myAlign=pA.getAlignment("A")
+            myAlign = pA.getAlignment("A")
             sys.stdout.write("Length   of alignment     = %d\n" % len(myAlign))
             ii = 0
             for myPr in myAlign:
-                if myPr[0] != myPr[1]:                
-                    sys.stdout.write("Py - conflict position %8d  -  %3s - %3s\n" % (ii,myPr[0],myPr[1]))
-                ii+=1
+                if myPr[0] != myPr[1]:
+                    sys.stdout.write("Py - conflict position %8d  -  %3s - %3s\n" % (ii, myPr[0], myPr[1]))
+                ii += 1
         except:
             traceback.print_exc(file=sys.stdout)
             self.fail()
@@ -118,19 +118,19 @@ class PairwiseAlignTests(unittest.TestCase):
         sys.stdout.write("Starting test function  %s" % sys._getframe().f_code.co_name)
         sys.stdout.write(" -------------------------\n")
         try:
-            pA=PairwiseAlign()
-            pA.setVerbose(self.__verbose)                        
-            pA.setReferenceSequence(self.seqRef3L,"REFA")
-            for k,v in self.sTests.items():
-                sys.stdout.write("Added test sequence %10s len = %8d\n" % (k,len(v)))
-                pA.addTestSequence(v,k)
+            pA = PairwiseAlign()
+            pA.setVerbose(self.__verbose)
+            pA.setReferenceSequence(self.seqRef3L, "REFA")
+            for k, v in self.sTests.items():
+                sys.stdout.write("Added test sequence %10s len = %8d\n" % (k, len(v)))
+                pA.addTestSequence(v, k)
             #
             sys.stdout.flush()
             myFails = pA.doAlignConsensus()
             sys.stdout.write("Failed sequences = %d\n" % len(myFails))
             sys.stdout.write("Length reference sequence = %d\n" % len(self.seqRef3L))
-            sys.stdout.flush() 
-            pA.wrAlignmentFull(ostream(sys.stdout))
+            sys.stdout.flush()
+            pA.prAlignmentFull()
         except:
             traceback.print_exc(file=sys.stdout)
             self.fail()
@@ -143,27 +143,27 @@ class PairwiseAlignTests(unittest.TestCase):
         sys.stdout.write("Starting test function  %s" % sys._getframe().f_code.co_name)
         sys.stdout.write(" -------------------------\n")
         try:
-            pA=PairwiseAlign()
-            pA.setVerbose(self.__verbose)                        
-            pA.setReferenceSequence(self.seqRef3L,"REFA")
+            pA = PairwiseAlign()
+            pA.setVerbose(self.__verbose)
+            pA.setReferenceSequence(self.seqRef3L, "REFA")
             sys.stdout.write("Length reference sequence = %d\n" % len(self.seqRef3L))
             #
-            for k,v in self.sTests.items():
-                sys.stdout.write("Added test sequence %10s len = %8d\n" % (k,len(v)))
-                pA.addTestSequence(v,k)
+            for k, v in self.sTests.items():
+                sys.stdout.write("Added test sequence %10s len = %8d\n" % (k, len(v)))
+                pA.addTestSequence(v, k)
             #
             sys.stdout.flush()
             myFails = pA.doAlignConsensus()
             sys.stdout.write("Failed sequences = %d\n" % len(myFails))
 
             sys.stdout.flush()
-            ofh=open("tmp-align-results.out",'w')
+            ofh = open("tmp-align-results.out", 'w')
             ofh.write("This example includes %d test sequences.\n" % len(self.sTests))
-            for k,v in self.sTests.items():            
+            for k, v in self.sTests.items():
                 ofh.write("Conflict list for case %s\n" % k)
-                pA.wrAlignmentConflicts(ostream(ofh),k)
-            ofh.write("Alignment Details \n")            
-            pA.wrAlignmentFull(ostream(ofh))
+                pA.prAlignmentConflicts(k)
+            ofh.write("Alignment Details \n")
+            pA.prAlignmentFull()
             ofh.write("Done\n")
             ofh.close()
         except:
@@ -172,9 +172,8 @@ class PairwiseAlignTests(unittest.TestCase):
 
 
 def suite():
-    return unittest.makeSuite(PairwiseAlignTests,'test')
+    return unittest.makeSuite(PairwiseAlignTests, 'test')
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
