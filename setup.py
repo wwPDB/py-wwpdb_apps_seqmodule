25e31407fe342dc542ac9803a91b2f6faa039f8a
Ezra Peisach <ezra.peisach@rcsb.org>
2019-06-09 07:22:45 -0400

    Add requirements for rcsb.utils.multiproc

--- setup.py
+++ setup.py
@@ -46,7 +46,8 @@ setup(
     install_requires=['wwpdb.utils.config >= 0.3', 'wwpdb.io', 'wwpdb.utils.session',
                       'wwpdb.utils.wf', 'wwpdb.utils.seqdb_v2 ~= 0.4.dev1',
                       'wwpdb.utils.align', 'wwpdb.utils.detach',
-                      'wwpdb.utils.dp', 'wwpdb.utils.cc_dict_util'],
+                      'wwpdb.utils.dp', 'wwpdb.utils.cc_dict_util',
+                      'rcsb.utils.multiproc'],
     packages=find_packages(exclude=['wwpdb.apps.tests-seqmodule',
                                     'mock-data']),
     # Enables Manifest to be used
